1a350bac9151a515a6552a10b30c34e8a9ffd3a1
Mick Sexton <lacrookedbeat@hotmail.com>
2017-09-09 14:22:34 -0700

    add first 2 buttons

---
+++ app/src/main/java/com/example/lacrookedbeat/positivitydose/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lacrookedbeat.positivitydose;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
